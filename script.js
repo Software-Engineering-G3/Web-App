9bc81a79e8b725fe444b3fd7222c31b3849378d6
Jens Vighagen <jvighagen@gmail.com>
2023-02-28 11:50:15 +0100

    Added toggleFan, toggleDoor and toggleRelay() and changed id="" for door, fan and relay

--- script.js
+++ script.js
@@ -9,6 +9,33 @@ function toggleAlarm() {
         console.log("Alarm Off");
     }
 }
+function toggleDoor() {
+    if (document.getElementById("door-checkbox").checked === true) {
+        socket.emit("Door-open");
+        console.log("Door Open");
+    } else {
+        socket.emit("Door-closed");
+        console.log("Door Closed");
+    }
+}
+function toggleFan() {
+    if (document.getElementById("fan-checkbox").checked === true) {
+        socket.emit("Fan-on");
+        console.log("Fan On");
+    } else {
+        socket.emit("Fan-off");
+        console.log("Fan Off");
+    }
+}
+function toggleRelay() {
+    if (document.getElementById("relay-checkbox").checked === true) {
+        socket.emit("Relay-on");
+        console.log("Relay On");
+    } else {
+        socket.emit("Relay-off");
+        console.log("Relay Off");
+    }
+}
 
 function play() {
     socket.emit("play");
