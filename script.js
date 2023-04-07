9ac8825e4933500f9898f44334412f2311aa2684
Alberto Fabbri <alberto_fabbri.git@yahoo.com>
2023-04-08 01:16:34 +0200

    Read sensor values on first connection

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ let connect = false;
 // Connection settings
 let url = "85.197.159.42";
 let port = "4121";
+let type = "http";
 
 // Write the settings to the page
 document.getElementById("url").innerHTML = url;
 document.getElementById("port").innerHTML = port;
+
+// Actuators
 let switchRelay = document.getElementById("relay-checkbox");
 let switchAlarm = document.getElementById("alarm-checkbox");
 let switchFan = document.getElementById("fan-checkbox");
@@ -17,6 +20,13 @@ let switchWindow = document.getElementById("window-checkbox");
 let switchOutdoorLight = document.getElementById("outdoor-light-checkbox");
 let sliderIndoorLight = document.getElementById("indoor-light-range");
 let switchDoor = document.getElementById("door-checkbox");
+
+// Sensors
+let gasDetector = document.getElementById("gas-detector-value");
+let ambientLight = document.getElementById("ambient-light-value");
+let motion = document.getElementById("motion-value");
+let soilHumidity = document.getElementById("soil-humidity-value");
+let steam = document.getElementById("steam-value");
 
 window.onload = function () {
     toggleConnection();
@@ -33,7 +43,7 @@ function toggleConnection() {
     if (connect === true) {
         // document.getElementById("connection-status").innerHTML = "Connecting...";
         //document.getElementById("connect-button").innerHTML = "Cancel";
-        socket = io.connect("https://" + url + ":" + port, {
+        socket = io.connect(type + "://" + url + ":" + port, {
             extraheaders: {
                 "Access-Control-Request-Private-Network": "true"
             }
@@ -59,37 +69,54 @@ function registerSocketEvents() {
     });
 
     socket.on('Info', (eventName, eventInfo) => {
-        eventName.forEach(actuator => {
+        eventName.forEach(device => {
+            // ACTUATORS
             // Door
-            if (actuator.component == "dr" && actuator.state == 1) {
+            if (device.component == "dr" && device.state == 1) {
                 switchDoor.checked = true;
             }
             // Window
-            if (actuator.component == "wi" && actuator.state == 1) {
+            if (device.component == "wi" && device.state == 1) {
                 switchWindow.checked = true;
             }
             // Buzzer
-            if (actuator.component == "bz" && actuator.state == 1) {
+            if (device.component == "bz" && device.state == 1) {
                 switchAlarm.checked = true;
             }
             // Fan
-            if (actuator.component == "fan" && actuator.state != 0) {
+            if (device.component == "fan" && device.state != 0) {
                 switchFan.checked = true;
-                sliderFan.value = actuator.state;
+                sliderFan.value = device.state;
             }
             // Indoor light
-            if (actuator.component == "il" && actuator.state == 1) {
-                sliderIndoorLight.value = actuator.state;
+            if (device.component == "il" && device.state == 1) {
+                sliderIndoorLight.value = device.state;
             }
             // Outdoor light
-            if (actuator.component == "ol" && actuator.state == 1) {
+            if (device.component == "ol" && device.state == 1) {
                 switchOutdoorLight.checked = true;
             }
             // Relay
-            if (actuator.component == "re" && actuator.state == 1) {
+            if (device.component == "re" && device.state == 1) {
                 switchRelay.checked = true;
             }
-            console.log(actuator);
+            // SENSORS
+            if (device.component == "light") {
+                ambientLight.innerHTML = device.state;
+            }
+            if (device.component == "gas") {
+                gasDetector.innerHTML = device.state;
+            }
+            if (device.component == "mot") {
+                motion.innerHTML = device.state;
+            }
+            if (device.component == "soil") {
+                soilHumidity.innerHTML = device.state;
+            }
+            if (device.component == "steam") {
+                steam.innerHTML = device.state;
+            }
+            console.log(device);
 
         });
     });
