6e0a0388f4d9140c4ce1736e32fedabe245e65c6
Alberto Fabbri <alberto_fabbri.git@yahoo.com>
2023-04-07 23:40:38 +0200

    Format

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ let port = "4121";
 // Write the settings to the page
 document.getElementById("url").innerHTML = url;
 document.getElementById("port").innerHTML = port;
-let switchRelay=document.getElementById("relay-checkbox");
-let switchAlarm=document.getElementById("alarm-checkbox");
-let switchFan=document.getElementById("fan-checkbox");
-let switchWindow=document.getElementById("window-checkbox");
-let switchOutdoorLamp=document.getElementById("outdoor-light-checkbox");
-let switchDoor=document.getElementById("door-checkbox");
+let switchRelay = document.getElementById("relay-checkbox");
+let switchAlarm = document.getElementById("alarm-checkbox");
+let switchFan = document.getElementById("fan-checkbox");
+let switchWindow = document.getElementById("window-checkbox");
+let switchOutdoorLamp = document.getElementById("outdoor-light-checkbox");
+let switchDoor = document.getElementById("door-checkbox");
 
-window.onload = function() {
+window.onload = function () {
     toggleConnection();
 };
 
@@ -24,12 +24,12 @@ function checkConnection() {
     //TODO check credentials, if they match we load the main page 
     window.location.replace("main.html");
 }
-  
+
 function toggleConnection() {
     connect = !connect;
     console.log(connect);
     if (connect === true) {
-       // document.getElementById("connection-status").innerHTML = "Connecting...";
+        // document.getElementById("connection-status").innerHTML = "Connecting...";
         //document.getElementById("connect-button").innerHTML = "Cancel";
         socket = io.connect("https://" + url + ":" + port, {
             extraheaders: {
@@ -58,33 +58,32 @@ function registerSocketEvents() {
 
     socket.on('Info', (eventName, eventInfo) => {
         eventName.forEach(actuators => {
-            if(actuators.component=="dr" && actuators.state==1){
-                switchDoor.checked=true;
+            if (actuators.component == "dr" && actuators.state == 1) {
+                switchDoor.checked = true;
             }
-            if(actuators.component=="wi" && actuators.state==1 ){
-                switchWindow.checked=true;
+            if (actuators.component == "wi" && actuators.state == 1) {
+                switchWindow.checked = true;
             }
-            if(actuators.component=="bz" && actuators.state==0 ){
-                switchAlarm.checked=true;
+            if (actuators.component == "bz" && actuators.state == 0) {
+                switchAlarm.checked = true;
             }
-            if(actuators.component=="fan" && actuators.state==0 ){
-                switchFan.checked=true;
-                
+            if (actuators.component == "fan" && actuators.state == 0) {
+                switchFan.checked = true;
+
             }
-            if(actuators.component=="il" && actuators.state==1 ){
+            if (actuators.component == "il" && actuators.state == 1) {
                 // TODO check for intensity with alberto 
             }
-            if(actuators.component=="ol" && actuators.state==1 ){
-                switchOutdoorLamp.checked=true;
+            if (actuators.component == "ol" && actuators.state == 1) {
+                switchOutdoorLamp.checked = true;
             }
-            if(actuators.component=="re" && actuators.state==1 ){
-                switchRelay.checked=true;
+            if (actuators.component == "re" && actuators.state == 1) {
+                switchRelay.checked = true;
             }
             console.log(actuators); // temporary to be deleted
-          
-        });
-      });
 
+        });
+    });
 }
 
 function play() {
@@ -154,10 +153,10 @@ const elements = [
     document.querySelector("#outdoor-light-checkbox-span"),
     document.querySelector("#outdoor-light-checkbox"),
     document.querySelector("#indoor-light-range"),
-]
+];
 
-for (let i = 0; i < elements.length; i ++) {
-    elements[i].addEventListener("click", function(event) { event.stopPropagation(); });
+for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", function (event) { event.stopPropagation(); });
 }
 
 
